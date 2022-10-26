bb90801bec80b3ecb22fbd51cfebcb64e3fe6626
Nikolas Nyby <nikolas@gnu.org>
2022-10-26 14:17:45 -0400

    Integrate django application
    Move backend application over from mathplayground_django repository.
    
    Once this is complete, I'll update the jenkins jobs to reference this
    repository instead of mathplayground_django.

---
+++ mathplayground/settings_staging.py
@@ -0,0 +1,44 @@
+from mathplayground.settings import *  # noqa: F403,F401
+
+project = 'mathplayground'
+s3prefix = 'ctl'
+
+# serve static files off S3
+AWS_STORAGE_BUCKET_NAME = s3prefix + "-" + project + "-static-stage"
+AWS_S3_OBJECT_PARAMETERS = {
+    'ACL': 'public-read',
+}
+AWS_PRELOAD_METADATA = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = ('https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME)
+
+MEDIA_URL = S3_URL + 'uploads/'
+AWS_LOCATION = 'media/'
+AWS_QUERYSTRING_AUTH = False
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django/' + project + '.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
+
+try:
+    from mathplayground.local_settings import *  # noqa: F403,F401
+except ImportError:
+    pass
---
+++ media/src/Polls.svelte
@@ -0,0 +1,142 @@
+<script>
+    import {
+        Button,
+        Modal,
+        ModalBody,
+        ModalFooter,
+        ModalHeader,
+        TabContent,
+        TabPane
+    } from 'sveltestrap';
+    import katex from 'katex';
+    import {makePoll} from './polls';
+    import {initialPolls} from './stores';
+
+    export let isPollsOpen, togglePolls;
+    export let polls = initialPolls;
+
+    const blankPreview = 'Question preview area (rendered LaTeX)';
+
+    const pollTextChange = function(e) {
+        const newVal = e.target.value;
+        let rendered;
+
+        if (newVal === '') {
+            rendered = blankPreview;
+        } else {
+            rendered = katex.renderToString(newVal);
+        }
+
+        const area = document.getElementById('pollPreviewArea');
+        area.innerHTML = rendered;
+    }
+
+    const clickMakePoll = function(e) {
+        e.preventDefault();
+        console.log('clickMakePoll', e);
+
+        const poll = makePoll(polls, 0, 'hi', null);
+        polls = [...polls, poll];
+
+        closePolls();
+    }
+
+    const closePolls = function() {
+        isPollsOpen = false;
+    }
+</script>
+
+<Modal isOpen={isPollsOpen} toggle={togglePolls} size="lg">
+    <ModalHeader toggle={togglePolls}>Polls</ModalHeader>
+    <ModalBody>
+        <TabContent>
+            <TabPane tabId="make-poll" tab="Make poll" active>
+                <form>
+                    <div class="mt-1 mb-3">
+                        Poll type:
+                        <br />
+                        <div class="form-check form-check-inline">
+                            <label class="form-check-label">
+                                <input class="form-check-input"
+                                       type="radio" name="pollType"
+                                       value="multiple-choice" checked>
+                                Multiple Choice
+                            </label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <label class="form-check-label">
+                                <input class="form-check-input"
+                                       type="radio" name="pollType"
+                                       value="point">
+                                Point
+                            </label>
+                        </div>
+                        <br />
+                        <div class="form-check form-check-inline">
+                            <label class="form-check-label">
+                                <input class="form-check-input"
+                                       type="radio" name="pollType"
+                                       value="free-text">
+                                Free text
+                            </label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <label class="form-check-label">
+                                <input class="form-check-input"
+                                       type="radio" name="pollType"
+                                       value="equation">
+                                Equation
+                            </label>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="pollQuestionTextarea" class="form-label">
+                            Ask a question
+                        </label>
+                        <textarea class="form-control"
+                                  on:input={pollTextChange}
+                                  id="pollQuestionTextarea"
+                                  aria-describedby="question"></textarea>
+
+                    </div>
+                    <p id="pollPreviewArea">
+                        {blankPreview}
+                    </p>
+                    <div class="mb-3">
+                        <input type="text" class="form-control"
+                               placeholder="Choice 1" aria-label="Choice 1">
+                        <input type="text" class="form-control"
+                               placeholder="Choice 2" aria-label="Choice 2">
+
+                        <Button color="secondary">
+                            + Add choice
+                        </Button>
+                    </div>
+                    <Button color="primary" on:click={clickMakePoll}>
+                        Make poll
+                    </Button>
+                </form>
+            </TabPane>
+            <TabPane tabId="view-polls" tab="View polls">
+                <ul>
+                    {#each polls as poll}
+                        <li>
+                            <a href={`/polls/${poll.id}/`}>Poll {poll.id}</a> -
+                            <strong>Prompts:</strong> {poll.prompt},
+                            {poll.choices.length} choices
+                        </li>
+                    {:else}
+                        No polls created!
+                    {/each}
+                </ul>
+            </TabPane>
+        </TabContent>
+
+    </ModalBody>
+
+    <ModalFooter>
+        <Button color="secondary" on:click={closePolls}>
+            Cancel
+        </Button>
+    </ModalFooter>
+</Modal>
