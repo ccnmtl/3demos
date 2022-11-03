bd8a073e5df50fe417071e998e7e053a7ea9ad5e
Nikolas Nyby <nikolas@gnu.org>
2022-11-03 14:06:16 -0400

    Remove unused django polls stuff

--- mathplayground/polls/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/polls/(?P<poll_id>\d+)/$',
-            consumers.PollsConsumer.as_asgi()),
-]
