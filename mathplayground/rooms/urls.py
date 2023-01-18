15732f6d890eb2e57fe51154dbf914ad6db8e8f4
Nikolas Nyby <nikolas@gnu.org>
2023-01-18 11:18:03 -0500

    WIP

--- mathplayground/rooms/urls.py
+++ mathplayground/rooms/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:room_id>/', views.room, name='room'),
+    path('<int:room_id>/admin',
+         views.RoomAdminView.as_view(),
+         name='room_admin'),
 ]
--- mathplayground/rooms/views.py
+++ mathplayground/rooms/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from mathplayground.rooms.scene import RedisScene
 
 
@@ -18,3 +19,13 @@ def room(request, room_id):
         'room_id': room_id,
         'scene_state': state,
     })
+
+
+class RoomAdminView(TemplateView):
+    template_name = 'admin.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        room_id = kwargs.get('room_id')
+        context['room_id'] = room_id
+        return context
