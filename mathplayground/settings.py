58a4557fcdb84359e1c347c52fc2de2c31704bfc
Nikolas Nyby <nikolas@gnu.org>
2023-01-27 13:19:37 -0500

    Use django-ga-context library to add GA_CODE to all views
    The GA code wasn't coming through in the room template, and I realized
    we have this library to automatically add this variable to all views.
    
    https://github.com/ccnmtl/django-ga-context

--- mathplayground/settings.py
+++ mathplayground/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
+                'gacontext.ga_processor',
             ],
         },
     },
