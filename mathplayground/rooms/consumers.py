99e6e75f0b9dab228b2dbca0204308d814054f48
Nikolas Nyby <nikolas@gnu.org>
2023-01-27 12:26:26 -0500

    Implement active user count UI

--- mathplayground/rooms/consumers.py
+++ mathplayground/rooms/consumers.py
@@ -6,6 +6,19 @@ from mathplayground.rooms.scene import RedisScene
 class RoomsConsumer(AsyncWebsocketConsumer):
     active_users = {}
 
+    # Send a room_event to everyone updating the active user count.
+    async def update_active_users(self, active_user_count, session_key):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'room_event',
+                'message': {
+                    'updateActiveUsers': active_user_count
+                },
+                'session_key': session_key,
+            }
+        )
+
     async def connect(self):
         self.room_id = self.scope['url_route']['kwargs']['room_id']
         self.room_group_name = 'room_%s' % self.room_id
@@ -22,6 +35,11 @@ class RoomsConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
+        # I've pretty much joined the room at this point. Send a
+        # message to everyone else with the new user count.
+        active_user_count = len(self.active_users[self.room_id])
+        await self.update_active_users(active_user_count, session_key)
 
         await self.accept()
 
@@ -40,6 +58,10 @@ class RoomsConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
+        # Update user count for this room
+        active_user_count = len(self.active_users[self.room_id])
+        await self.update_active_users(active_user_count, session_key)
 
     # Receive message from WebSocket
     async def receive(self, text_data):
@@ -101,7 +123,9 @@ class RoomsConsumer(AsyncWebsocketConsumer):
         session = self.scope['session']
 
         # Don't send events to myself.
-        if event.get('session_key') == session.session_key:
+        # Well, except for the updateActiveUsers event.
+        if 'updateActiveUsers' not in message and \
+           event.get('session_key') == session.session_key:
             return
 
         # Send out the update to any room participants
