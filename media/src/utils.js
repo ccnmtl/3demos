85df77339871ac1669e9bf646d49b9c68ae9345a
Drew C Youngren <dcy2@columbia.edu>
2023-05-25 19:03:49 -0400

    Fix rectangular sides resolution

--- media/src/utils.js
+++ media/src/utils.js
@@ -1816,32 +1816,27 @@ class RectangularSolidGeometry extends THREE.BufferGeometry {
         base = points.length / 3;
 
         for (let i = 0; i <= nX; i++) {
-            points.push(
-                a + i * dx,
-                c(a + i * dx),
-                e(a + i * dx, c(a + i * dx)))
+            const z0 = e(a + i * dx, c(a + i * dx));
+            const z1 = f(a + i * dx, c(a + i * dx));
             vec.set(
                 c(a + i * dx + dt2) - c(a + i * dx - dt2),
                 -dt,
                 0
             ).normalize()
-            normals.push(vec.x, vec.y, vec.z)
-
-            points.push(
-                a + i * dx,
-                c(a + i * dx),
-                f(a + i * dx, c(a + i * dx)))
-            vec.set(
-                c(a + i * dx + dt2) - c(a + i * dx - dt2),
-                -dt,
-                0
-            ).normalize()
-            normals.push(vec.x, vec.y, vec.z)
-
+            for (let j = 0; j <= nX; j++) {
+                points.push(
+                    a + i * dx,
+                    c(a + i * dx),
+                    z0 + j / nX * (z1 - z0))
+                normals.push(vec.x, vec.y, vec.z)
+            }
         }
         for (let i = 0; i < nX; i++) {
-            indices.push(base + i * (2), base + i * 2 + 2, base + i * 2 + 1)
-            indices.push(base + i * 2 + 2, base + i * 2 + 3, base + i * 2 + 1)
+            for (let j = 0; j < nX; j++) {
+                indices.push(base + (i * (nX + 1) + j)
+                    , base + ((i + 1) * (nX + 1) + j), base + (i * (nX + 1) + j) + 1)
+                indices.push(base + ((i + 1) * (nX + 1) + j), base + ((i + 1) * (nX + 1) + j) + 1, base + (i * (nX + 1) + j) + 1)
+            }
         }
 
         // back
@@ -1849,56 +1844,54 @@ class RectangularSolidGeometry extends THREE.BufferGeometry {
         base = points.length / 3;
 
         for (let i = 0; i <= nX; i++) {
-            points.push(
-                a + i * dx,
-                d(a + i * dx),
-                e(a + i * dx, d(a + i * dx)))
+            const z0 = e(a + i * dx, d(a + i * dx));
+            const z1 = f(a + i * dx, d(a + i * dx));
             vec.set(
-                d(a + i * dx - dt2) - d(a + i * dx + dt2),
-                dt,
+                d(a + i * dx + dt2) - d(a + i * dx - dt2),
+                -dt,
                 0
             ).normalize()
-            normals.push(vec.x, vec.y, vec.z)
-
-            points.push(
-                a + i * dx,
-                d(a + i * dx),
-                f(a + i * dx, d(a + i * dx)))
-            vec.set(
-                d(a + i * dx - dt2) - d(a + i * dx + dt2),
-                dt,
-                0
-            ).normalize()
-            normals.push(vec.x, vec.y, vec.z)
-
+            for (let j = 0; j <= nX; j++) {
+                points.push(
+                    a + i * dx,
+                    d(a + i * dx),
+                    z0 + j / nX * (z1 - z0))
+                normals.push(vec.x, vec.y, vec.z)
+            }
         }
         for (let i = 0; i < nX; i++) {
-            indices.push(base + i * (2), base + i * 2 + 1, base + i * 2 + 2)
-            indices.push(base + i * 2 + 2, base + i * 2 + 1, base + i * 2 + 3)
+            for (let j = 0; j < nX; j++) {
+                indices.push(base + (i * (nX + 1) + j)
+                    , base + ((i + 1) * (nX + 1) + j), base + (i * (nX + 1) + j) + 1)
+                indices.push(base + ((i + 1) * (nX + 1) + j), base + ((i + 1) * (nX + 1) + j) + 1, base + (i * (nX + 1) + j) + 1)
+            }
         }
 
         // left
 
         base = points.length / 3;
+
         dy = (d(a) - c(a)) / nY
         for (let i = 0; i <= nY; i++) {
-            points.push(
-                a,
-                c(a) + i * dy,
-                e(a, c(a) + i * dy)
-            )
-            normals.push(-1, 0, 0)
+            const z0 = e(a, c(a) + i * dy);
+            const z1 = f(a, c(a) + i * dy);
 
-            points.push(
-                a,
-                c(a) + i * dy,
-                f(a, c(a) + i * dy)
-            )
-            normals.push(-1, 0, 0)
+            for (let j = 0; j <= nX; j++) {
+
+                points.push(
+                    a,
+                    c(a) + i * dy,
+                    z0 + j / nX * (z1 - z0)
+                )
+                normals.push(-1, 0, 0)
+            }
         }
         for (let i = 0; i < nY; i++) {
-            indices.push(base + i * (2), base + i * 2 + 1, base + i * 2 + 2)
-            indices.push(base + i * 2 + 2, base + i * 2 + 1, base + i * 2 + 3)
+            for (let j = 0; j < nX; j++) {
+                indices.push(base + (i * (nX + 1) + j)
+                    , base + ((i + 1) * (nX + 1) + j), base + (i * (nX + 1) + j) + 1)
+                indices.push(base + ((i + 1) * (nX + 1) + j), base + ((i + 1) * (nX + 1) + j) + 1, base + (i * (nX + 1) + j) + 1)
+            }
         }
 
         // right
@@ -1906,23 +1899,24 @@ class RectangularSolidGeometry extends THREE.BufferGeometry {
         base = points.length / 3;
         dy = (d(b) - c(b)) / nY
         for (let i = 0; i <= nY; i++) {
-            points.push(
-                b,
-                c(b) + i * dy,
-                e(b, c(b) + i * dy)
-            )
-            normals.push(1, 0, 0)
+            const z0 = e(b, c(b) + i * dy);
+            const z1 = f(b, c(b) + i * dy);
 
-            points.push(
-                b,
-                c(b) + i * dy,
-                f(b, c(b) + i * dy)
-            )
-            normals.push(1, 0, 0)
+            for (let j = 0; j <= nX; j++) {
+                points.push(
+                    b,
+                    c(b) + i * dy,
+                    z0 + j / nX * (z1 - z0)
+                )
+                normals.push(1, 0, 0)
+            }
         }
         for (let i = 0; i < nY; i++) {
-            indices.push(base + i * (2), base + i * 2 + 2, base + i * 2 + 1)
-            indices.push(base + i * 2 + 2, base + i * 2 + 3, base + i * 2 + 1)
+            for (let j = 0; j < nX; j++) {
+                indices.push(base + (i * (nX + 1) + j)
+                    , base + ((i + 1) * (nX + 1) + j), base + (i * (nX + 1) + j) + 1)
+                indices.push(base + ((i + 1) * (nX + 1) + j), base + ((i + 1) * (nX + 1) + j) + 1, base + (i * (nX + 1) + j) + 1)
+            }
         }
 
         this.setAttribute('position', new THREE.Float32BufferAttribute(points, 3))
