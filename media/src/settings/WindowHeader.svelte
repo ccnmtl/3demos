d6df40ca131189592ac41e873404cd1fb3baa0a2
Evan Petersen <emp2221@columbia.edu>
2023-06-22 11:08:01 -0400

    Focus jumps to the top of the chosen menu and back if the 'X' button is clicked

--- media/src/settings/WindowHeader.svelte
+++ media/src/settings/WindowHeader.svelte
@@ -1,6 +1,10 @@
 <script>
     export let title = '';
     export let onClick = function() {};
+
+    const loadFocus = function (el) {
+        el.focus();
+    }
 </script>
 <div class="row">
     <div class="col-auto me-auto">
@@ -11,7 +15,9 @@
             type="button"
             title="Close"
             class="btn btn-sm btn-light"
-            on:click={onClick}>
+            on:click={onClick}
+            use:loadFocus
+        >
             <i class="bi bi-x-lg" />
         </button>
     </div>
