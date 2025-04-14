a3e6b115f0600ed2a64bfbe2883b5254e6d96255
Drew Youngren <dcy2@columbia.edu>
2025-04-14 17:29:13 -0400

    Remove debugging

--- media/src/stories/Blank.svelte
+++ media/src/stories/Blank.svelte
@@ -6,9 +6,3 @@
     This is Story Mode. It is in development. Choose a topic to learn more
     theory, see guided examples, and try out exercises.
 </p>
-
-{#if animate}
-    <p>animate found</p>
-{:else}
-    <p>No animate.</p>
-{/if}
